c6c558559621d1eaffe8f9bed8e71fbaf7e066d0
Taher Abouzeid <tabouzeid@gmail.com>
2020-08-11 21:31:00 -0400

    lint config file

---
+++ server.js
@@ -0,0 +1,20 @@
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+const express = require('express');
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}`);
+});
