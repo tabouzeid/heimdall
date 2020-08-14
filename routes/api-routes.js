914d0583ddd8dc53ec0f1cb90181efb758ba7e26
Taher Abouzeid <tabouzeid@gmail.com>
2020-08-13 22:52:42 -0400

    placeholders for api-routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,5 +2,30 @@
 const db = require('../models');
 
 module.exports = function (app) {
+  app.get('/api/product', (req, res) => {
+    // return contents of the Product table
+    res.end();
+  });
 
+  app.post('/api/product', (req, res) => {
+    // add a new row to the Product table
+    res.end();
+  });
+
+  app.put('/api/product', (req, res) => {
+    // update an existing row in the Product table
+    res.end();
+  });
+
+  app.get('/api/product/:sku', (req, res) => {
+    // return Product table info for req.params.sku
+    // as well as the 3rd party api info for that sku
+    res.end();
+  });
+
+  app.post('/api/order', (req, res) => {
+    // create a new entry in the Order table and get the orderId for it
+    // add each item in your order to the OrderDetail table with the orderId included on each item
+    res.end();
+  });
 };
