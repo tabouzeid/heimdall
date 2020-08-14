29ef7b6aae48dfe242e34ba1bff502cf7395834a
Taher Abouzeid <tabouzeid@gmail.com>
2020-08-13 23:21:57 -0400

    linting product schema

---
+++ models/product.js
@@ -0,0 +1,43 @@
+module.exports = function (sequelize, DataTypes) {
+  const Product = sequelize.define('Product', {
+    sku: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+      validate: {
+        len: [1],
+      },
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    inventoryQuantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    currentPurchasePrice: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    currentSalePrice: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    minRequirement: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  });
+  return Product;
+};
