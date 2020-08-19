f2c9818ed339845389ea50ea6630e30ed9ad94cc
samson wong <steelerwong2005@hotmail.com>
2020-08-19 02:29:31 -0400

    API lookup funciton, and add inv to newInv handlebars, almost there

--- public/js/skuLookup.js
+++ public/js/skuLookup.js
@@ -1,20 +1,58 @@
 /* eslint-disable no-plusplus */
 /* eslint-disable no-use-before-define */
+
 $(document).ready(() => {
-  $('#sku-search-btn').on('click', () => {
-    // save the category they typed into the category-search input
+  $('#sku-search-btn').on('click', (event) => {
+    event.preventDefault();
+
     const searchCategory = $('#category-search').val().trim();
-    console.log('user type serach cateogry', searchCategory);
     searchProduct(searchCategory);
   });
+
+  // $('.add-inv-btn').on('click', (event) => {
+  //   save the category they typed into the category-search input
+  //   Retrieve the value of that event, save in the object
+  //   console.log('user clicked on add this item', event);
+  //   window.location.replace('/add/inventory');
+  // });
+
+  // $(document).on('click', '.add-inv-btn', addSupplierInventory);
+
+  $(document).on('click', '.add-inv-btn', function (event) {
+    event.preventDefault();
+    // Extract the data-inventory value associated with that Button
+    // This data-inventory ID value is the same, as other sku-id, name-id value.
+    // use jQuery to search for that sku-id element;if found, extract that text.
+    const myVal = $(this).data('inventory');
+
+    const suppInvData = {
+      sku: $(`#sku-${myVal}`).text(),
+      name: $(`#name-${myVal}`).text(),
+      productDesc: $(`#desc-${myVal}`).text(),
+      sellPrice: $(`#saleprice-${myVal}`).text(),
+    };
+
+    window.location.replace('/add/inventory');
+    addSupplierInventory(suppInvData);
+  });
 });
+
+function addSupplierInventory(newInventory) {
+  // event.preventDefault();
+  console.log('Triggered add supplier inventory with SKU', newInventory.sku);
+  console.log($('input#sku'));
+  // $('input#sku').text(newInventory.sku);
+  // $('input#sku').value = newInventory.sku;
+  // $('input#sku').html(newInventory.sku);
+  $('input#sku').val(newInventory.sku);
+  // $('#sku').val("111122223333");
+}
 
 function searchProduct(category) {
   const homeAudioCategoryID = 'pcmcat241600050001';
   const categoryIs = {
     'Home Audio': 'pcmcat241600050001',
   };
-  console.log('Inside search product, category', category);
 
   // const categoryURL = 'https://api.bestbuy.com/v1/products((categoryPath.id=pcmcat241600050001))?apiKey=vDloch7HfMAIbPtlLB2FE6Sp&sort=image.asc&show=categoryPath.name,image,name,salePrice,shortDescription,sku,thumbnailImage&format=json&pageSize=100';
   const categoryURL = `https://api.bestbuy.com/v1/products((categoryPath.id=${homeAudioCategoryID}))?apiKey=vDloch7HfMAIbPtlLB2FE6Sp&sort=image.asc&show=categoryPath.name,image,name,salePrice,shortDescription,sku,thumbnailImage&format=json&pageSize=100`;
@@ -26,8 +64,8 @@ function searchProduct(category) {
 }
 
 function productResult(productObj) {
-  console.log('Retrieved product resutls is', productObj);
-  console.log('Size of the array:', productObj.products.length);
+  // console.log('Retrieved product resutls is', productObj);
+  // console.log('Size of the array:', productObj.products.length);
 
   // for each products that API replied back
   for (let i = 0; i < productObj.to; i++) {
@@ -39,14 +77,19 @@ function productResult(productObj) {
     productSection.attr('id', `product-well-${i}`);
     // append the well to the well section
     $('#product-section').append(productSection);
-    console.log('In the end of FOR loop:', i, 'product section is:', productSection);
+    // console.log('In the end of FOR loop:', i, 'product section is:', productSection);
 
     // Now add all of product data to the well we just placed on the page
-    console.log('Before append, the product is: ', productObj.products[i]);
+    // Below, use jQUERY way, to find, for those ID equal to this product-well-i, then
+    // Append the content to it. this is the same as
+    // <div id = product-well-xxx>
+    // -> add the sku, name, description..etc, via the append method
+    // </div>
 
-    $(`#product-well-${i}`).append(`<h2>SKU: ${productObj.products[i].sku}</h2>`);
-    $(`#product-well-${i}`).append(`<h2>Name: ${productObj.products[i].name}</h2>`);
-    $(`#product-well-${i}`).append(`<h2>Description: ${productObj.products[i].shortDescription}</h2>`);
-    $(`#product-well-${i}`).append(`<h2>SalePrice: ${productObj.products[i].salePrice}</h2>`);
+    $(`#product-well-${i}`).append(`<h2 id=sku-${i}>${productObj.products[i].sku}</h2>`);
+    $(`#product-well-${i}`).append(`<h2 id=name-${i}>${productObj.products[i].name}</h2>`);
+    $(`#product-well-${i}`).append(`<h2 id=desc-${i}>${productObj.products[i].shortDescription}</h2>`);
+    $(`#product-well-${i}`).append(`<h2 id=saleprice-${i}>${productObj.products[i].salePrice}</h2>`);
+    $(`#product-well-${i}`).append(`<button class="btn btn-primary add-inv-btn" data-inventory="${i}">Add This Inventory</button>`);
   }
 }
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -16,7 +16,7 @@ module.exports = function (app) {
   app.get('/add/inventory', (req, res) => {
     res.render('newInventory');
   });
-  
+
   app.get('/add/skuLookup', (req, res) => {
     res.render('skuLookup');
   });
