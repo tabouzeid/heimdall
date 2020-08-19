8fb20e99de2b81a926e71817b2f3caf2b02f83d6
Taher Abouzeid <tabouzeid@gmail.com>
2020-08-20 00:24:48 +0200

    changing schema

--- models/order.js
+++ models/order.js
@@ -9,13 +9,6 @@ module.exports = function (sequelize, DataTypes) {
         len: [1],
       },
     },
-    clientName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1],
-      },
-    },
     date: {
       type: DataTypes.DATE,
       allowNull: false,
